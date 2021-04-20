c5766778199070559b3af5433b54d3694c933a95
Jeferson Schimuneck <jefersonschimuneck@gmail.com>
2021-04-20 17:11:10 -0300

    Fix challenge 1 lint issue

--- desafio1.sql
+++ desafio1.sql
@@ -1,5 +1,8 @@
-SELECT COUNTRY_NAME AS `País`,
-IF(REGION_ID = 1, 'incluído', 'não incluído') AS `Status Inclusão`
-FROM hr.countries
+SELECT 
+    COUNTRY_NAME AS `País`,
+    IF(REGION_ID = 1,
+        'incluído',
+        'não incluído') AS `Status Inclusão`
+FROM
+    hr.countries
 ORDER BY COUNTRY_NAME;
-

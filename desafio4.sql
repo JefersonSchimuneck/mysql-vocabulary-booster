1b4db3dea929173dee8d6e52bcefaed82e0efe4d
Jeferson Schimuneck <jefersonschimuneck@gmail.com>
2021-04-20 18:27:31 -0300

    Complete challenges 2, 3, 4

---
+++ desafio4.sql
@@ -0,0 +1,15 @@
+SELECT 
+    J.JOB_TITLE AS `Cargo`,
+    ROUND(AVG(E.SALARY), 2) AS `Média salarial`,
+    CASE
+        WHEN ROUND(AVG(E.SALARY), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
+        WHEN ROUND(AVG(E.SALARY), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
+        WHEN ROUND(AVG(E.SALARY), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
+        ELSE 'CEO'
+    END AS 'Senioridade'
+FROM
+    hr.employees AS E
+        INNER JOIN
+    hr.jobs AS J ON E.JOB_ID = J.JOB_ID
+GROUP BY J.JOB_TITLE
+ORDER BY `Média salarial` , J.JOB_TITLE;

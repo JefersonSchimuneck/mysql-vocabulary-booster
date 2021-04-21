82abb1502fa9238ac2f741e4170abba6dd3cd4bc
Jeferson Schimuneck <jefersonschimuneck@gmail.com>
2021-04-20 21:44:34 -0300

    Fix challenge 9

--- desafio9.sql
+++ desafio9.sql
@@ -1,9 +1,9 @@
 SELECT 
     CONCAT(E.FirstName, ' ', E.LastName) AS `Nome completo`,
-    COUNT(*) AS `Total de Pedidos`
+    COUNT(*) AS `Total de pedidos`
 FROM
     w3schools.orders AS O
         INNER JOIN
     w3schools.employees AS E ON O.EmployeeID = E.EmployeeID
 GROUP BY `Nome Completo`
-ORDER BY `Total de Pedidos`;
+ORDER BY `Total de pedidos`;
